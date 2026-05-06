861f374227ac7ca1de9b242d327153c0f6073ef3
Yaahi Gupta <iknowyashi30@gmail.com>
2026-05-06 17:05:28 +0530

    Update errorMiddleware.js

--- backend/src/middleware/errorMiddleware.js
+++ backend/src/middleware/errorMiddleware.js
@@ -17,11 +17,8 @@ const handleValidationErrorDB = err => {
   return new AppError(message, 400);
 };
 
-const handleJWTError = () => new AppError('Invalid token. Please log in again!', 401);
-
-const handleJWTExpiredError = () => new AppError('Your token has expired! Please log in again.', 401);
-
 const globalErrorHandler = (err, req, res, next) => {
+  console.log('Error Handler caught:', err);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
@@ -36,13 +33,10 @@ const globalErrorHandler = (err, req, res, next) => {
     // Production
     let error = { ...err };
     error.message = err.message;
-    error.name = err.name;
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
-    if (error.name === 'JsonWebTokenError') error = handleJWTError();
-    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
     if (error.isOperational) {
       res.status(error.statusCode).json({
